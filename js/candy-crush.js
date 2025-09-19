22b5068004b9c27e4db7bf4749dac41f262ca781
smeet-hsv99 <smeethirpara99@gmail.com>
2025-09-19 08:17:11 +0530

    Candy Crush Game Fixed -Addon

--- js/candy-crush.js
+++ js/candy-crush.js
@@ -4,6 +4,9 @@ var rows = 9;
 var columns = 9;
 var score = 0;
 
+var currTile;
+var otherTile;
+
 window.onload = function() {
     startGame();
 }
@@ -20,9 +23,65 @@ function startGame() {
             tile.id = r.toString() + "-" + c.toString();
             tile.src = "./images/Candy-crush/" + randomCandy() + ".png";
 
+            tile.addEventListener("dragstart", dragStart);
+            tile.addEventListener("dragover", dragOver);
+            tile.addEventListener("dragenter", dragEnter);
+            tile.addEventListener("dragleave", dragLeave);
+            tile.addEventListener("drop", dragDrop);
+            tile.addEventListener("dragend", dragEnd);
+
             document.getElementById("board").append(tile);
             row.push(tile);
         }
         board.push(row);
     }
+}
+
+function dragStart() {
+    currTile = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave(e) {
+
+}
+
+function dragDrop(e) {
+    otherTile = this;
+}
+
+function dragEnd(e) {
+    let currCoords = currTile.id.split('-');
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split('-');
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let moveLeft = c2 == c-1 && r == r2;
+    let moveRight = c2 == c+1 && r == r2;
+
+    let moveUp = r2 == r-1 && c == c2;
+    let moveDown = r2 == r+1 && c == c2;
+
+    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+    }
+}
+
+function crushCandy() {
+
 }
