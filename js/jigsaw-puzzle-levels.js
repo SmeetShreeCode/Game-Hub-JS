70199f83282c87a35cd56a2eba2f99e02fcf3df4
SmeetShreeCode <smeet@shreecode.com>
2025-12-09 18:21:19 +0530

    9th, December 2025 Tuesday Last Commit -Done

---
+++ js/jigsaw-puzzle-levels.js
@@ -0,0 +1,15 @@
+const IMAGE_PATH = "images/Jigsaw-puzzle/";
+const chapters = {
+    levels: [
+        {image: `${IMAGE_PATH}L1.jpg`},
+        {image: `${IMAGE_PATH}L2.jpg`},
+        {image: `${IMAGE_PATH}L3.jpg`},
+        {image: `${IMAGE_PATH}L4.jpg`},
+        {image: `${IMAGE_PATH}L5.jpg`},
+        {image: `${IMAGE_PATH}L6.jpg`},
+        {image: `${IMAGE_PATH}L7.jpg`},
+        {image: `${IMAGE_PATH}L8.jpg`},
+        {image: `${IMAGE_PATH}L9.jpg`},
+        {image: `${IMAGE_PATH}L10.jpg`},
+    ],
+};
---
+++ js/puzzle-game-levels.js
@@ -0,0 +1,15 @@
+const IMAGE_PATH = "images/Puzzle-craft/";
+const chapters = {
+    levels: [
+        {image: `${IMAGE_PATH}L1.webp`},
+        {image: `${IMAGE_PATH}L2.jpg`},
+        {image: `${IMAGE_PATH}L3.webp`},
+        {image: `${IMAGE_PATH}L4.webp`},
+        {image: `${IMAGE_PATH}L5.webp`},
+        {image: `${IMAGE_PATH}L6.webp`},
+        {image: `${IMAGE_PATH}L7.webp`},
+        {image: `${IMAGE_PATH}L8.webp`},
+        {image: `${IMAGE_PATH}L9.webp`},
+        {image: `${IMAGE_PATH}L10.webp`},
+    ],
+};
